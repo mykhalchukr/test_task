c22c65013363901cd0d405ae085384f84bb02013
Roma <roman.mykhalchuk@yahoo.com>
2020-06-16 23:19:15 +0300

    Updates

---
+++ precache-manifest.e93d4197dacf81a8b1be9013fe2db415.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "74131ae0b0f54a7a3673d526c91df7d7",
+    "url": "/test_task/index.html"
+  },
+  {
+    "revision": "ef602d30193de8c4c0c1",
+    "url": "/test_task/static/css/main.ccf2526b.chunk.css"
+  },
+  {
+    "revision": "50878493491897de989f",
+    "url": "/test_task/static/js/2.7e801c1f.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/test_task/static/js/2.7e801c1f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ef602d30193de8c4c0c1",
+    "url": "/test_task/static/js/main.c13754b8.chunk.js"
+  },
+  {
+    "revision": "f78bcdf00a9ab863d9d5",
+    "url": "/test_task/static/js/runtime-main.b1555d5b.js"
+  }
+]);
--- src/helpers/api.js
+++
@@ -1,6 +0,0 @@
-import axios from 'axios';
-const URL = 'https://randomuser.me/api/?results=50';
-
-export const getDataFromServer = () => {
-  return axios.get(URL);
-}
